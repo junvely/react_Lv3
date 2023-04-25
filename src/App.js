c9571c8b5bcc4cdd23e8e03f0cde04d01704d384
junvely <junvely97@gmail.com>
2023-04-25 10:03:43 +0900

    Feat : 폴더 및 컴포넌트 구조 설계

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import styled from "styled-components";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <StSection>
+        <StTitle>Button</StTitle>
+        {/* buttons */}
+      </StSection>
+      <StSection>
+        <StTitle>Input</StTitle>
+        {/* inputs */}
+      </StSection>
+      <StSection>
+        <StTitle>Modal</StTitle>
+        {/* modals */}
+      </StSection>
+      <StSection>
+        <StTitle>Select</StTitle>
+        {/* selects */}
+      </StSection>
+    </>
+  );
 }
+
+const StSection = styled.section``;
+const StTitle = styled.h1`
+  font-size: 30px;
+`;
 
 export default App;
